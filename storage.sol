17c3cc8b96799ada208eb4feb36eff457d786805
kemsguy7 <mattidungafa@gmail.com>
2025-11-29 23:25:46 +0100

    updates: started working with storage arrays

--- storage.sol
+++ storage.sol
@@ -3,7 +3,9 @@
 pragma solidity >=0.8.0;
 
 contract Storage {
-  uint256 internal x;
+  uint256 internal x; //internal means other smart contracts cannot see the value
+
+  uint256[] public myArray;
 
   function setX(uint256 newValue) public {
     x = newValue;
@@ -12,4 +14,11 @@ contract Storage {
   function getX() public view returns (uint256) {
     return x;
   }
+
+  // Arrays in storage
+  function setMyArray(uint256[] calldata newArray) public {
+    myArray = newArray;
+  }
+
+  function addToArray(uint256 newItem) public {}
 }
