275e73bb2fd3cfb50b048fc3d8e7051cb605d28c
kemsguy7 <mattidungafa@gmail.com>
2025-12-06 06:22:45 +0100

    Started working with mappings

---
+++ mappings.sol
@@ -0,0 +1,38 @@
+// mappings, Hashmaps, associative array, maps.
+
+contract ExampleContract {
+  mapping(uint256 => uint256) public myMapping;
+
+  // - If you access a mapping with a key that has not been set, you will NOT get a revert , the mapping will just return the Zero value  of the datatype for the value
+
+  // returns false by default
+  mapping(uint256 => bool) public mapToBool;
+
+  //returns 0 by default
+  mapping(uint256 => uint256) public mapToUint;
+
+  // returns ox000000000000000000000000000000000000000000 by default
+  mapping(uint256 => address) public mapToAddress;
+
+  function setMapping(uint256 key, uint256 value) public {
+    myMapping[key] = value;
+  }
+
+  function getValue(uint256 key) public view returns (uint256) {
+    return myMapping[key];
+  }
+}
+
+// flaw: anyone can invoke the public functions and send tokens : to be fixed later
+contract ERC20Token {
+  mapping(address => uint256) public balances;
+
+  function setSomeonesBalances(address owner, uint256 amount) public {
+    balances[owner] = amount;
+  }
+
+  function transferTokensBetweenAddresses(address sender, address reciever, uint256 amount) public {
+    balances[sender] -= amount; //deduct/debut the sender's balance
+    balances[reciever] += amount; //credit the receiver's balance
+  }
+}
